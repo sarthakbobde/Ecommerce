7be577acff6edc9603d50e079ad2d9ad75acbdd0
Sarthak <bobdesarthak@gmail.com>
2025-01-20 23:30:22 +0530

    Initial Commit

---
+++ ecom/base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    
+    
+    class Meta:
+        abstract = True # so that django doesn't this as a model but it will treat as a class so we can use it in etire project
